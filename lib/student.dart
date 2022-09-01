0945286780576b44331fa13a48205a69266b32d3
Girts <girts.strazdins@gmail.com>
2022-09-01 13:50:18 +0200

    Student auto-numbering

--- lib/student.dart
+++ lib/student.dart
@@ -1,11 +1,24 @@
 /// Represents a student
 class Student {
+  static int studentCount = 0;
+
+  final int id;
+
   final Set<Student> friends = {};
+
+  Student() : id = ++studentCount;
 
   /// Register a new friendship
   void addFriend(Student friend) {
+    // For debugging - print out this, then you see connections between students
+    // print("$this -> $friend");
     friends.add(friend);
     // Let's make sure the friendship is symmetrical
     friend.friends.add(this);
+  }
+
+  @override
+  String toString() {
+    return "Student_$id";
   }
 }
